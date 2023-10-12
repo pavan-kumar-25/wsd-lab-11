7004f22190057433a9cfc771cc392aec077f7c18
Pavan Kumar C <141422505+pavan-kumar-25@users.noreply.github.com>
2023-10-12 10:20:20 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function TaskList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+    const [editedTask, setEditedTask] = useState(null);
+    const [editedText, setEditedText] = useState('');
+
+    const addTask = () => {
+        if (newTask.trim() !== '') {
+            setTasks([...tasks, { id: Date.now(), text: newTask }]);
+            setNewTask('');
+        }
+    };
+
+    const editTask = (id) => {
+        const taskToEdit = tasks.find((task) => task.id === id);
+        if (taskToEdit) {
+            setEditedTask(id);
+            setEditedText(taskToEdit.text);
+        }
+    };
+
+    const saveTask = (id) => {
+        const updatedTasks = tasks.map((task) =>
+            task.id === id ? { ...task, text: editedText } : task
+        );
+        setTasks(updatedTasks);
+        setEditedTask(null);
+    };
+
+    const deleteTask = (id) => {
+        const updatedTasks = tasks.filter((task) => task.id !== id);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div className="task-list">
+            <h1>Task List</h1>
+            <div>
+                <input
+                    className='input-task'
+                    type="text"
+                    placeholder="Add a new task"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                />
+                <button className="btnadd" onClick={addTask}>Add</button>
+            </div>
+
+                <ul>
+                    {tasks.map((task) => (
+                        <li key={task.id}>
+                            {editedTask === task.id ? (
+                                <div>
+                                    <input
+                                        className='input-edit'
+                                        type="text"
+                                        value={editedText}
+                                        onChange={(e) => setEditedText(e.target.value)}
+                                    />
+                                    <button className='btn-save' onClick={() => saveTask(task.id)}>Save</button>
+                                </div>
+                            ) : (
+                                <div className='ul-list'> 
+                                    {task.text}
+                                    <button className="btn-edit" onClick={() => editTask(task.id)}>Edit</button>
+                                    <button className="btn-delete" onClick={() => deleteTask(task.id)}>Delete</button>
+                                </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+    );
 }
 
-export default App;
+export default TaskList;
